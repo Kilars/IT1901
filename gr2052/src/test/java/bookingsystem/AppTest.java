dcb0cc8836e6f8f6e84f9149db83fd6f437b7c46
Lars Skifjeld <larsski@stud.ntnu.no>
2020-09-14 13:53:50 +0000

    La til tester

--- gr2052/src/test/java/bookingsystem/AppTest.java
+++ gr2052/src/test/java/bookingsystem/AppTest.java
@@ -1,7 +1,11 @@
 package bookingsystem;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import bookingsystem.core.User;
 
 
 /**
@@ -17,4 +21,28 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void testreadFromFile()
+    {
+        //readFromFile returns void,to test we need them to return the values not println them
+    }
+
+    @Test
+    public void testUser()
+    {
+        User user = new User();
+        user.setPhone("12345678");
+        assertEquals("12345678", user.getPhone());
+        //Throw exception instead of print or both?
+/*         try {
+            user.setSurname("lars34");
+            fail();
+        } catch (Exception e) {
+            
+        } */
+        user.setEmail("larsski@gmail.com");
+        assertEquals("larsski@gmail.com", user.getEmail());
+    }
+
 }
