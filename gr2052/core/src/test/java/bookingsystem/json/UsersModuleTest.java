19e0267225595c666c7b13b0bbf0a2047e745b77
Magnus Holta <magnholt@stud.ntnu.no>
2020-10-11 23:42:20 +0000

    #65_started on usermoduletest

--- gr2052/core/src/test/java/bookingsystem/json/UsersModuleTest.java
+++ gr2052/core/src/test/java/bookingsystem/json/UsersModuleTest.java
@@ -1,12 +1,45 @@
 package bookingsystem.json;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 import bookingsystem.core.Booking;
+import bookingsystem.core.HairDresser;
+import bookingsystem.core.Treatment;
 import bookingsystem.core.User;
 import bookingsystem.core.Users;
+
+/*
+Format
+{"users" : [
+    (User) {
+        "firstName" : "Ola",
+        "surname" : "Nordmann",
+        "email" : "ola.nordmann@mail.com",
+        "phone" : "12345678",
+        "password" : "passord01",
+        "bookings" : [
+            (Booking) {
+                "treatment" : (Treatment) {
+                    "treatment" : "herreklipp"
+                    "price" : (Double) "100"
+                },
+                "hairDresser" : (HairDresser) {
+                    "name" : "Kari Nordmann"
+                },
+                "date" : (String) "...",
+                "time" : "10:30"
+            }
+        ]
+    }
+]} 
+ */
 
 /** Test for checking UserModule
  * Checking one case
@@ -14,39 +47,33 @@ import bookingsystem.core.Users;
 public class UsersModuleTest {
 
     public static ObjectMapper mapper;
+    private static String jsonSample = "{\"users\":[{\"firstName\":\"Ola\",\"surname\":\"Nordmann\",\"email\":\"ola.nordmann@mail.no\",\"phone\":\"12345678\",\"password\":\"Heiheih1832\"},{\"firstName\":\"Kari\",\"surname\":\"Nordmann\",\"email\":\"kari.nordmann@mail.no\",\"phone\":\"12345678\",\"password\":\"Heiheih1832\"}]}";
+    private Users users;
+    private User user1, user2;
+    private Booking booking1, booking2;
+    private HairDresser hairdresser;
+    private Treatment treatment1, treatment2;
+    private LocalDate date;
+    private String time;
 
     @BeforeAll
-    public static void setUp() {
+    public static void init() {
         mapper = new ObjectMapper();
         mapper.registerModule(new UsersModule());
     }
 
-    /*
-    Format
-    {"users" : [
-        (User) {
-            "firstName" : "Ola",
-            "surname" : "Nordmann",
-            "email" : "ola.nordmann@mail.com",
-            "phone" : "12345678",
-            "password" : "passord01",
-            "bookings" : [
-                (Booking) {
-                    "treatment" : (Treatment) {
-                        "treatment" : "herreklipp"
-                        "price" : (Double) "100"
-                    },
-                    "hairDresser" : (HairDresser) {
-                        "name" : "Kari Nordmann"
-                    },
-                    "date" : (String) "...",
-                    "time" : "10:30"
-                }
-            ]
-        }
-    ]} 
-     */
-    private final static String jsonSample = "{\"users\":[{\"firstName\":\"Ola\",\"surname\":\"Nordmann\",\"email\":\"ola.nordmann@mail.no\",\"phone\":\"12345678\",\"password\":\"Heiheih1832\"},{\"firstName\":\"Kari\",\"surname\":\"Nordmann\",\"email\":\"kari.nordmann@mail.no\",\"phone\":\"12345678\",\"password\":\"Heiheih1832\"}]}";
+    @BeforeEach
+    public void setUp() {
+        users = new Users();
+        user1 = new User();
+        user2 = new User();
+        booking1 = new Booking();
+        booking2 = new Booking();
+        hairdresser = new HairDresser("Frisør");
+        treatment1 = new Treatment("Dameklipp", 299);
+        treatment2 = new Treatment("Dameklipp", 299);
+        date = LocalDate.of(2020, 12, 25);
+    }
 
     @Test
     public void testSerializers() {
