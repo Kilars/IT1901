043a2357b4913ea398aac80331b498002167b919
Magnus Holta <magnholt@stud.ntnu.no>
2020-10-29 20:34:28 +0000

    #121_update core pom

---
+++ bookingsystem/api/src/main/java/bookingsystem/service/BookingService.java
@@ -0,0 +1,14 @@
+package bookingsystem.service;
+
+import bookingsystem.core.users;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+  
+  public Users users;
+
+  public static void main(String[] args) {
+    System.out.println("hei");
+  }
+}
