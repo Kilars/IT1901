f84cbebfa192f7a01662627f87b81c2b2ff4c781
Magnus Holta <magnholt@stud.ntnu.no>
2020-10-29 19:58:59 +0000

    #121_added rest module to start implementing spring boot

---
+++ bookingsystem/rest/src/main/java/bookingsystem/core/BookingServiceApplication.java
@@ -0,0 +1,12 @@
+package bookingsystem.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookingServiceApplication.class, args);
+  }
+}
